3a6bf0a822ba0c147f1238059b243c202ef1c2b9
aldinh777 <aldi.nh789@gmail.com>
2024-03-22 11:17:34 +0800

    remove validation because there is already a feature for it called "in"

--- object/validate.js
+++
@@ -1,8 +0,0 @@
-/**
- * Checks if an object has any specified keys
- *
- * @param {any} obj
- * @param  {...string} keys
- * @returns {boolean}
- */
-export const has = (obj, ...keys) => keys.every((key) => obj && typeof obj === 'object' && Reflect.has(obj, key));
